8a9cacabd8d4235737f466d23e03ec14a96ba29f
David Arias <davariasc@udistrital.edu.co>
2025-11-26 13:06:16 -0500

    acciones

--- patients/viewsets.py
+++ patients/viewsets.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import PatientSerializer
 from .models import Patient
 
 class PatientViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
+
+    @action(["GET"], detail=True, url_path="set-name")
+    def custom_operation(self, request,pk):
+        patient = self.get_object()
+        if patient.first_name == "David":
+            patient.first_name = "Juan"
+        patient.save()
+        return Response("Nombre modificado")
